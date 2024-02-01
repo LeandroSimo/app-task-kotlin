899f0396d8940a62da9fcf3c5ade124eaa5f9fcd
LeandroSimo <leo.bt.12@hotmail.com>
2024-02-01 00:39:26 -0300

    feat: creating and configuring tabLayout

---
+++ app/src/main/java/com/leandro/taskapp/ui/DoneFragment.kt
@@ -0,0 +1,31 @@
+package com.leandro.taskapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leandro.taskapp.R
+import com.leandro.taskapp.databinding.FragmentDoneBinding
+
+
+class DoneFragment : Fragment() {
+
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
