bbfb4c49c553a6ccd58e8d33ac1ca22dd6c7a8f6
LeandroSimo <leo.bt.12@hotmail.com>
2024-02-02 21:58:11 -0300

    feat: creating layout form tasks

---
+++ app/src/main/java/com/leandro/taskapp/ui/FormTaskFragment.kt
@@ -0,0 +1,32 @@
+package com.leandro.taskapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leandro.taskapp.R
+import com.leandro.taskapp.databinding.FragmentFormTaskBinding
+
+class FormTaskFragment : Fragment() {
+
+    private var _binding: FragmentFormTaskBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFormTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/leandro/taskapp/ui/TodoFragment.kt
+++ app/src/main/java/com/leandro/taskapp/ui/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.leandro.taskapp.R
 import com.leandro.taskapp.databinding.FragmentTodoBinding
 
@@ -22,6 +23,19 @@ class TodoFragment : Fragment() {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.floatingActionButton.setOnClickListener {
+           findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
